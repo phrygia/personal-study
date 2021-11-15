307f19b1c4d20ddd2af5286efeaa461c9bef62b9
chaeyeonLee <dmsgp62@gmail.com>
2021-11-16 00:10:35 +0900

    문자열압축 문제

---
+++ 프로그래머스/Level2/문자열압축.js
@@ -0,0 +1,9 @@
+function solution(s) {
+  var answer = 0;
+  return answer;
+}
+console.log(solution("aabbaccc"));
+console.log(solution("ababcdcdababcdcd"));
+console.log(solution("abcabcdede"));
+console.log(solution("abcabcabcabcdededededede"));
+console.log(solution("xababcdcdababcdcd"));
