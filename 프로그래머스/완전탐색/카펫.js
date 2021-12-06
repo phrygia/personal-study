26d7d783ce6361e3fe7f5d4b96539c3aa9258b81
chaeyeonLee <dmsgp62@gmail.com>
2021-12-06 20:37:06 +0900

    탐욕법 파일 생성

--- 프로그래머스/완전탐색/카펫.js
+++ 프로그래머스/완전탐색/카펫.js
@@ -1,6 +1,18 @@
 // https://programmers.co.kr/learn/courses/30/lessons/42842?language=javascript
 
-function solution(brown, yellow) {}
-console.log(solution(10, 2));
-console.log(solution(8, 1));
+function solution(brown, yellow) {
+  let answer = [];
+  const sum = brown + yellow;
+
+  for (let height = 3; height <= brown; height++) {
+    if (sum % height === 0) {
+      let weight = sum / height; // 가로길이
+      console.log(sum, weight, height);
+      if ((height - 2) * (weight - 2) === yellow) return [weight, height];
+    }
+  }
+  return answer;
+}
+// console.log(solution(10, 2));
+// console.log(solution(8, 1));
 console.log(solution(24, 24));
---
+++ 프로그래머스/탐욕법/체육복.js
@@ -0,0 +1,9 @@
+// https://programmers.co.kr/learn/courses/30/lessons/42862?language=javascript
+
+function solution(n, lost, reserve) {
+  let answer = n;
+  return answer;
+}
+console.log(solution(5, [2, 4], [1, 3, 5]));
+console.log(solution(5, [2, 4], [3]));
+console.log(solution(3, [3], [1]));
