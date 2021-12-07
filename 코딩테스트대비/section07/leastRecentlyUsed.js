c357242751470629d3c59b8ed598b2b809d7595a
Lee Chaeyeon <dmsgp62@gmail.com>
2021-12-08 02:10:02 +0900

    인프런 정렬 알고리즘

---
+++ 코딩테스트대비/section07/leastRecentlyUsed.js
@@ -0,0 +1,2 @@
+const soluction = (arr) => {};
+console.log(soluction([11, 7, 5, 6, 10, 9]));
--- 코딩테스트대비/section07/선택정렬.js
+++ 코딩테스트대비/section07/선택정렬.js
@@ -1,21 +1,23 @@
 const soluction = (arr) => {
-  const length = arr.length;
-  let indexMin;
+    const length = arr.length;
+    let idx;
 
-  for (let i = 0; i < length - 1; i++) {
-    indexMin = i;
-    for (let j = i; j < length; j++) {
-      console.log("indexMin : ", indexMin, arr[indexMin], " j : ", j, arr[j]);
-      if (arr[indexMin] > arr[j]) indexMin = j;
+    for (let i = 0; i < length - 1; i++) {
+        idx = i;
+        for (let j = i; j < length; j++) {
+            // console.log("indexMin : ", indexMin, arr[indexMin], " j : ", j, arr[j]);
+            if (arr[idx] > arr[j]) idx = j;
+        }
+        // console.log('-----------', i, indexMin);
+
+        if (i !== idx) {
+            [arr[i], arr[idx]] = [arr[idx], arr[i]];
+            // const aux = arr[i];
+            // arr[i] = arr[indexMin];
+            // arr[indexMin] = aux;
+        }
     }
-    console.log("-----------", i, indexMin);
-    if (i !== indexMin) {
-      const aux = arr[i];
-      arr[i] = arr[indexMin];
-      arr[indexMin] = aux;
-    }
-  }
-  return arr;
+    return arr;
 };
 console.log(soluction([13, 5, 11, 7, 23, 15]));
 
