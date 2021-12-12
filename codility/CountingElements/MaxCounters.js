b507a43384a29e99b200ed3a2fad2bad6a195d3a
chaeyeonLee <dmsgp62@gmail.com>
2021-12-12 18:02:00 +0900

    maxcounter 문제

--- codility/CountingElements/MaxCounters.js
+++ codility/CountingElements/MaxCounters.js
@@ -1,17 +1,25 @@
 function solution(N, A) {
   let counter = Array(N).fill(0);
+  let max = 0;
+  let lastMax = 0;
 
   for (let i = 0; i < A.length; i++) {
-    if (A[i] > N) {
-      console.log(A[i], N);
-      //   const maxCount = Math.max.apply(null, counter);
-      //   counter = Array(N).fill(maxCount);
-    } else {
-      // counter[A[i] - 1]++;
-    }
+    const x = A[i];
+    if (x <= N) {
+      if (counter[x - 1] < lastMax) counter[x - 1] = lastMax;
+
+      counter[x - 1]++;
+
+      if (max < counter[x - 1]) max = counter[x - 1];
+    } else lastMax = max;
   }
 
+  for (let i = 0; i < counter.length; i++) {
+    if (counter[i] < lastMax) counter[i] = lastMax;
+  }
   return counter;
 }
 console.log(solution(5, [3, 4, 4, 6, 1, 4, 4]));
 // console.log(solution())
+
+// (3, 2, 2, 4, 2)
