75e26a6c575b93e166c1564e5b4df1c55ed3b76a
phrygia <dmsgp62@gmail.com>
2021-12-16 01:12:19 +0900

    맥 13꺼

--- 프로그래머스/완전탐색/모의고사.js
+++ 프로그래머스/완전탐색/모의고사.js
@@ -1,5 +1,27 @@
 // https://programmers.co.kr/learn/courses/30/lessons/42840?language=javascript
 
-function solution(answers) {}
+function solution(answers) {
+	let people = [
+		[1, 2, 3, 4, 5], // 5
+		[2, 1, 2, 3, 2, 4, 2, 5], //8
+		[3, 3, 1, 1, 2, 2, 4, 4, 5, 5], // 10
+	];
+
+	let answer = answers.reduce(
+		(acc, cur, idx) => {
+			if (cur === people[0][idx % 5]) acc[0]++;
+			if (cur === people[1][idx % 8]) acc[1]++;
+			if (cur === people[2][idx % 10]) acc[2]++;
+			return acc;
+		},
+		[0, 0, 0]
+	);
+
+	const max = Math.max.apply(null, answer);
+	return answer.reduce((acc, cur, idx) => {
+		if (cur === max) acc.push(idx + 1);
+		return acc;
+	}, []);
+}
 console.log(solution([1, 2, 3, 4, 5]));
 console.log(solution([1, 3, 2, 4, 2]));
--- 프로그래머스/완전탐색/카펫.js
+++ 프로그래머스/완전탐색/카펫.js
@@ -1,6 +1,34 @@
 // https://programmers.co.kr/learn/courses/30/lessons/42842?language=javascript
 
-function solution(brown, yellow) {}
-console.log(solution(10, 2));
-console.log(solution(8, 1));
+function solution(brown, yellow) {
+	// 가로 + 세로
+	const sum = Math.floor(brown / 2) + 2;
+
+	// 세로길이 <= 가로길이
+	// brown / 2 = 가로 + 세로 - 2
+	// 가로 + 세로 = brown / 2 + 2
+	for (let i = 3; 2 * i <= sum; i++) {
+		console.log(i, sum, i - 2, sum - i - 2);
+		if ((i - 2) * (sum - i - 2) === yellow) {
+			return [sum - i, i];
+		}
+	}
+}
+// console.log(solution(10, 2));
+// console.log(solution(8, 1));
 console.log(solution(24, 24));
+
+/*
+[0,0] [0,1] [0,2]
+[1,0] [1,1] [1,2]
+[2,0] [2,1] [2,2]
+
+
+
+[0,0] [0,1] [0,2] [0,3] [0,4] [0,5] [0,6] [0,7]
+[1,0] [1,1] [1,2] [1,3] [1,4] [1,5] [1,6] [1,7] 
+[2,0] [2,1] [2,2] [2,3] [2,4] [2,5] [2,6] [2,7] 
+[3,0] [3,1] [3,2] [3,3] [3,4] [3,5] [3,6] [3,7] 
+[4,0] [4,1] [4,2] [4,3] [4,4] [4,5] [4,6] [4,7] 
+[5,0] [5,1] [5,2] [5,3] [5,4] [5,5] [5,6] [5,7] 
+*/
