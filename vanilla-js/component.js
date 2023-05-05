90513f065df99736be3b661c5d1873e0f9d51a49
phrygia <dmsgp62@gmail.com>
2023-05-05 15:23:32 +0900

    스터디 파일 통합

---
+++ vanilla-js/component.js
@@ -0,0 +1,5 @@
+/*
+https://junilhwang.github.io/TIL/Javascript/Design/Vanilla-JS-Component/#_1-%E1%84%89%E1%85%A1%E1%86%BC%E1%84%90%E1%85%A2%E1%84%80%E1%85%AA%E1%86%AB%E1%84%85%E1%85%B5%E1%84%8B%E1%85%B4-%E1%84%90%E1%85%A1%E1%86%AB%E1%84%89%E1%85%A2%E1%86%BC
+
+state가 변하지 않을 경우 DOM이 변하면 안됨
+*/
