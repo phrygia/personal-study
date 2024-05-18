e089133f71f6eefa387b6fdf2c50bde504ed1941
phrygia <dmsgp62@gmail.com>
2024-05-18 15:01:08 +0900

    알고리즘 공부

--- algorithm/udemy/이진트리/BST_Classes.js
+++ algorithm/udemy/이진트리/BST_Classes.js
@@ -15,15 +15,73 @@ class BinarySearchTree {
     if (this.root === null) {
       this.root = newNode;
       return this;
-    } else {
     }
+
+    let current = this.root;
+    while (true) {
+      if (value === current.value) return undefined;
+      if (value < current.value) {
+        // current보다 작으니 왼쪽
+        if (current.left === null) {
+          current.left = newNode;
+          return this;
+        }
+        current = current.left;
+      } else if (value > current.value) {
+        // current보다 크니 오른쪽
+        if (current.right === null) {
+          current.right = newNode;
+          return this;
+        }
+        current = current.right;
+      }
+    }
+  }
+  contains(value) {
+    if (this.root === null) return false;
+    let current = this.root;
+    let found = false;
+
+    while (current && !found) {
+      if (value < current.value) {
+        current = current.left;
+      } else if (value > current.value) {
+        current = current.right;
+      } else {
+        // 값 찾음
+        return true;
+      }
+    }
+    return false;
+  }
+  BFS() {
+    let node = this.root;
+    const data = [];
+    const queue = [];
+
+    queue.push(node);
+
+    while (queue.length) {
+      node = queue.shift();
+      data.push(node.value);
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+    return data;
+  }
+  DFSPreOrder() {
+    //
   }
 }
 
 var tree = new BinarySearchTree();
-tree.root = new Node(10);
-tree.root.right = new Node(15);
-tree.root.left = new Node(7);
-tree.root.left.right = new Node(9);
-
-console.log(tree);
+tree.insert(10);
+tree.insert(6);
+tree.insert(15);
+tree.insert(3);
+tree.insert(8);
+tree.insert(20);
+tree.insert(7);
+tree.insert(3);
+console.log(tree.BFS());
+// console.log(tree);
